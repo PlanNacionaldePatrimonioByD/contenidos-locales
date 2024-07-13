b05264f8ebce4f243d77568f667b39b9438826a8
Jc-Arroyo <jcarroyos@gmail.com>
2024-07-13 00:13:06 +0000

    avance en agregar vista de retícula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
+import { RouterOutlet, RouterModule } from '@angular/router'; // Importa RouterModule aquí
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatRippleModule } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    RouterModule, // Añade RouterModule aquí
     RouterOutlet,
     MatButtonModule,
+    MatGridListModule,
+    MatRippleModule
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'contenidos-locales';
